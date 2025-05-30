a3216ffad7e2c6ec62e718b7bb68efc66c4dc718
Jishnu-phani <jishnu.phani@gmail.com>
2025-05-30 20:50:09 +0530

    fixed install state field of json output in 2.py

--- 2.py
+++ 2.py
@@ -281,6 +281,10 @@ def extract_firmware_log_info(log_path, uuid, info_dict):
                 match = re.search(r'FirmwareDriverBaselineSettings on server .*? is (.*)', line)
                 if match:
                     install_state = match.group(1).strip()
+                    print(f"Install state found: {install_state}")
+                    pattern = r'"State"\s*:\s*"([^"]*)"'
+                    install_state = re.search(pattern, install_state)
+                    install_state = install_state.group(1) if install_state else "Unknown"
                     break
 
         info_dict["Firmware Update"]["SPP Used"] = spp_used
